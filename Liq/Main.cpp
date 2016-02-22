19f0ff8f4cdcce0a88151f0734e71930c27b9846
Boris Town <tbziy@hotmail.com>
2016-02-22 15:52:45 +0800

    Change Function searchValue

--- Liq/Main.cpp
+++ Liq/Main.cpp
@@ -8,7 +8,6 @@ int searchValue(char** Array, char* Value, int** targets)
 	int targetCounts = 0;
 	char** CurStr = Array;
 	(*targets) = (int*)malloc(sizeof(int));
-	(*targets)[0] = 0;
 	while (CurStr)
 	{
 		if (strcmp(*CurStr, Value))
@@ -16,9 +15,9 @@ int searchValue(char** Array, char* Value, int** targets)
 			targetCounts++;
 			(*targets) = (int*)realloc(targets, sizeof(int) * (targetCounts + 1));
 			(*targets)[targetCounts - 1] = CurStr - Array;
-			(*targets)[targetCounts] = 0;
 		}
 		CurStr ++;
 	}
+	(*targets)[targetCounts] = 0;
 	return targetCounts;
 }
